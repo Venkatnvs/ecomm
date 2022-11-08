c8d77e359f3992ec3b08a6649033e1c9ed112e9b
Venkatnvs <venkatnvs2005@gmail.com>
2022-11-08 15:36:39 +0530

    ctm player

--- videos/urls.py
+++ videos/urls.py
@@ -5,5 +5,5 @@ urlpatterns= [
     path('', main, name='video-main'),
     path('v/', main2, name='video-main2'),
     path('v/2/', main3, name='video-main3'),
-    path('v/2/', ctm_play, name='video-ctm'),
+    path('c/', ctm_play, name='video-ctm'),
 ]
