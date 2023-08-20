8a7c785ad2794e5b78834c7046ca76ee5520d783
N.Venkat Swaroop <venkatnvs2005@gmail.com>
2023-08-21 00:57:11 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@ set -o errexit
 
 pip install -r requirements.txt
 npm install webpack webpack-cli webpack-obfuscator --save-dev
+npm install javascript-obfuscator --save-dev
+npm fund
 npx webpack --config webpack.config.js
 python manage.py makemigrations
 python manage.py migrate
